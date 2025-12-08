982a23ed4a7170be34dc75e8482c7fce81dc4097
maiyihe <2656377727@qq.com>
2025-12-08 16:42:42 +0800

    添加 tab 缩进快捷键

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -1,21 +1,24 @@
 -- 撤销键:ctrl+z
-vim.keymap.set({"n","i"}, "<C-z>", "<Cmd>undo<CR>", {silent = true})
+vim.keymap.set({ "n", "i" }, "<C-z>", "<Cmd>undo<CR>", { silent = true })
 
 -- 命令行模式：候选菜单弹出时用 ↑ / ↓ 选择补全
 vim.keymap.set("c", "<Down>", function()
-  return vim.fn.pumvisible() == 1 and "<C-n>" or "<Down>"
+	return vim.fn.pumvisible() == 1 and "<C-n>" or "<Down>"
 end, { expr = true })
 
 vim.keymap.set("c", "<Up>", function()
-  return vim.fn.pumvisible() == 1 and "<C-p>" or "<Up>"
+	return vim.fn.pumvisible() == 1 and "<C-p>" or "<Up>"
 end, { expr = true })
-
 
 -- 自动格式化快捷键
 vim.keymap.set("n", "<leader>f", function()
-  require("conform").format({
-    async = true,
-    lsp_format = "fallback",
-  })
-   end, { desc = "Format file" })
+	require("conform").format({
+		async = true,
+		lsp_format = "fallback",
+	})
+end, { desc = "Format file" })
 
+-- Visual 模式 Tab 缩进 → 右缩进，且保持选区
+vim.keymap.set("v", "<Tab>", ">gv", { desc = "Visual indent right" })
+-- Visual 模式 Shift+Tab 缩进 → 左缩进，且保持选区
+vim.keymap.set("v", "<S-Tab>", "<gv", { desc = "Visual indent left" })
